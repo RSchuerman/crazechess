e00ffa9dc5b94b30146159e4b3f5d583097b9c37
RSchuerman <reed.schuerman@trojans.dsu.edu>
2023-12-01 16:49:55 -0600

    chess.

--- src/components/Board/Pieces/Pieces.js
+++
@@ -1,8 +0,0 @@
-import "./Pieces.css";
-
-const Pieces = () => {
-  const position = new Array(8).fill("").map((x) => new Array(8).fill(""));
-  console.log(position);
-};
-
-export default Pieces;
--- src/components/Board/bits/Ranks.js
+++ src/components/Board/bits/Ranks.js
@@ -1,13 +1,11 @@
 import "./Ranks.css";
 
-const Ranks = ({ ranks }) => {
-  return (
-    <div className="ranks">
-      {ranks.map((rank) => (
-        <span key={rank}>{rank}</span>
-      ))}
-    </div>
-  );
-};
+const Ranks = ({ ranks }) => (
+  <div className="ranks">
+    {ranks.map((rank) => (
+      <span key={rank}>{rank}</span>
+    ))}
+  </div>
+);
 
 export default Ranks;
